cf44589c9b7e81f1f435c009d452f0d6ec1c5f5b
Kashyap Sangani <kashyapsangani@Kashyaps-MacBook-Air.local>
2025-09-25 16:45:44 +0100

    feat: update mobile menu icon toggle and close on outside click

--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -2,9 +2,25 @@
 window.addEventListener('DOMContentLoaded', function() {
   var menuBtn = document.getElementById('mobile-menu-btn');
   var menuList = document.getElementById('mobile-menu-list');
+
   if (menuBtn && menuList) {
     menuBtn.addEventListener('click', function() {
       menuList.classList.toggle('open');
+      if (menuList.classList.contains('open')) {
+        menuBtn.innerHTML = 'Close &#10005;'; // Change to cross icon
+        document.body.addEventListener('click', closeMenuOnClickOutside);
+      } else {
+        menuBtn.innerHTML = 'Menu &#9776;'; // Change back to menu icon
+        document.body.removeEventListener('click', closeMenuOnClickOutside);
+      }
     });
+
+    function closeMenuOnClickOutside(event) {
+      if (!menuList.contains(event.target) && event.target !== menuBtn) {
+        menuList.classList.remove('open');
+        menuBtn.innerHTML = 'Menu &#9776;';
+        document.body.removeEventListener('click', closeMenuOnClickOutside);
+      }
+    }
   }
 });
